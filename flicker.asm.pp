b9342926c97be510323f44177113d81dc8daf607
Lyren Brown <lyren.brown@gmail.com>
2014-05-26 18:59:03 -0500

    use START/SELECT to adjust luma of hue frame

--- flicker.asm.pp
+++ flicker.asm.pp
@@ -2,10 +2,12 @@
     org $80
 tmp org *+1
 pri org *+1
+bak org *+1
 
     org $2000
     sei
     mva #0 NMIEN
+    sta bak
     sta DMACTL
     lda PORTB
     and #$FE
@@ -38,7 +40,29 @@ vbi
     lda #$80
     eor:sta pri
     sta PRIOR
+    lda CONSOL
+    and #1
+    cmp:sta laststart
+    scs:inc bak
+    lda CONSOL
+    and #2
+    cmp:sta lastselect
+    scs:dec bak
+    lda pri
+    bpl black
+    lda bak
+    and #7
+    asl @
+    sta COLBAK
     jmp donenmi
+black
+    mva #0 COLBAK
+    jmp donenmi
+
+laststart
+    dta 1
+lastselect
+    dta 2
 
 
     org $4000
